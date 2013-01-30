f5c7751a3960720ce4fdc62d6a1ab4b6e547a6de
Felix Mo <felix@felixmo.com>
2013-01-30 16:00:03 -0500

    'mwapi' now utilizes the network activity indicator

--- Commons-iOS/mwapi/Http.m
+++ Commons-iOS/mwapi/Http.m
@@ -7,6 +7,8 @@
 //
 
 #import "Http.h"
+#import "MWNetworkActivityIndicatorManager.h"
+
 
 @implementation Http
 
@@ -33,10 +35,14 @@
     data_ = [[NSMutableData alloc] init];
     connection_ = [[NSURLConnection alloc] initWithRequest:requestUrl_ delegate:self];
     [connection_ start];
+    
+    [[MWNetworkActivityIndicatorManager sharedManager] show];
 }
 
 - (void)cancel {
     [connection_ cancel];
+    
+    [[MWNetworkActivityIndicatorManager sharedManager] hide];
 }
 
 #pragma mark - NSConnectionDelegate methods
@@ -50,6 +56,9 @@
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
     NSLog(@"finished loading");
+    
+    [[MWNetworkActivityIndicatorManager sharedManager] hide];
+    
     if (onCompletion_ != nil) {
         NSError *error;
         MWApiResult *result = [[MWApiResult alloc]initWithRequest:requestUrl_ response:response_ responseBody:data_ errors:error];
@@ -72,7 +81,7 @@
 
 -(void) connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
 {
-    
+    [[MWNetworkActivityIndicatorManager sharedManager] hide];
 }
 
 - (void) connection:(NSURLConnection *)connection didSendBodyData:(NSInteger)bytesWritten totalBytesWritten:(NSInteger)totalBytesWritten totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite
