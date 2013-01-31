45afdc79c8603e82141cedf2fbc2d019199edc0b
Daniel Zhang <daniel@danielzhang.info>
2013-01-30 22:22:35 -1000

    advance text field with 'next' and dismiss keyboard with 'done'

--- Commons-iOS/SettingsViewController.h
+++ Commons-iOS/SettingsViewController.h
@@ -8,7 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SettingsViewController : UIViewController
+@interface SettingsViewController : UIViewController <UITextFieldDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
 @property (weak, nonatomic) IBOutlet UISwitch *debugModeSwitch;
