2ac0da9939941d4b0e74a30805a66e24e293d118
Daniel Zhang <daniel@danielzhang.info>
2013-02-02 23:42:52 -1000

    setup support for application unit testing along with a shared scheme; tests are enabled in Commons-iOS target

---
+++ Commons-iOS-Application-Testing/Commons_iOS_Application_Testing.m
@@ -0,0 +1,32 @@
+//
+//  Commons_iOS_Application_Testing.m
+//  Commons-iOS-Application-Testing
+//
+//  Created by Daniel Zhang (張道博) on 2/2/13.
+//  Copyright (c) 2013 Wikimedia. All rights reserved.
+//
+
+#import "Commons_iOS_Application_Testing.h"
+
+@implementation Commons_iOS_Application_Testing
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Commons-iOS-Application-Testing");
+}
+
+@end
