320b92124855082ac51895d04410708e43c8d19e
Daniel Zhang <daniel@danielzhang.info>
2013-02-02 21:48:33 -1000

    added support for logic unit testing as an independent target using OCUnit and OCMock

---
+++ Commons-iOS-Logic-Testing/Commons_iOS_Logic_Testing.m
@@ -0,0 +1,32 @@
+//
+//  Commons_iOS_Logic_Testing.m
+//  Commons-iOS-Logic-Testing
+//
+//  Created by Daniel Zhang (張道博) on 2/2/13.
+//  Copyright (c) 2013 Wikimedia. All rights reserved.
+//
+
+#import "Commons_iOS_Logic_Testing.h"
+
+@implementation Commons_iOS_Logic_Testing
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Commons-iOS-Logic-Testing");
+}
+
+@end
