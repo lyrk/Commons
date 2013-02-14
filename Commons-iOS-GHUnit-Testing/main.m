eb602a131a68d006116a0a1a7f97f07ece1ec660
Daniel Zhang <daniel@danielzhang.info>
2013-02-13 23:09:29 -1000

    adding ghunit target

---
+++ Commons-iOS-GHUnit-Testing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Commons-iOS-GHUnit-Testing
+//
+//  Created by Daniel Zhang (張道博) on 2/13/13.
+//  Copyright (c) 2013 Wikimedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
