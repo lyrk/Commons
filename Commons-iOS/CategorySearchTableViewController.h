e83fa72ad4489fecd14d041e6a69e22004e1ca16
Brion Vibber <brion@pobox.com>
2013-04-19 13:39:47 -0700

    stub controller for cat search

---
+++ Commons-iOS/CategorySearchTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  CategorySearchTableViewController.h
+//  Commons-iOS
+//
+//  Created by Brion on 4/19/13.
+//  Copyright (c) 2013 Wikimedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategorySearchTableViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@end
