33429a63d4b7d1b644880313dce8580a8de2457d
Monte Hurd <mhurd@wikimedia.org>
2013-06-03 17:17:32 -0700

    Async image of day retrieval. Better caching. Faster json data access.

--- Commons-iOS/PictureOfTheDay.m
+++ Commons-iOS/PictureOfTheDay.m
@@ -12,8 +12,6 @@
 #define POTD_MAX_W_H_RATIO 2.0f
 
 @implementation PictureOfTheDay{
-    NSURL *potdURL_;
-    UIImage *potdImage_;
     ThumbFetcher *thumbFetcher_;
 }
 
@@ -22,10 +20,17 @@
     self = [super init];
     if (self) {
         thumbFetcher_ = [[ThumbFetcher alloc] init];
-        potdURL_ = nil;
-        potdImage_ = nil;
     }
     return self;
+}
+
+-(NSString *)potdCacheFileName
+{
+    NSDate *date = [[NSDate alloc] init];
+    date = [date dateByAddingTimeInterval: -(86400.0 * POTD_DAYS_AGO)];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyy-MM-dd"];
+    return [NSString stringWithFormat:@"POTD-%@", [formatter stringFromDate:date]];
 }
 
 -(NSURL *)getJsonUrl
@@ -36,83 +41,60 @@
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
     [formatter setDateFormat:@"yyyy-MM-dd"];
     NSString *urlStr = [NSString stringWithFormat:
-    @"http://commons.wikimedia.org/w/api.php?action=query&generator=images&prop=imageinfo&titles=Template:Potd/%@&iiprop=url|size|comment|metadata|user|userid&format=json", [formatter stringFromDate:date]];
+        @"http://commons.wikimedia.org/w/api.php?action=query&generator=images&prop=imageinfo&titles=Template:Potd/%@&iiprop=url|size|comment|metadata|user|userid&format=json", [formatter stringFromDate:date]];
     
-    urlStr = [urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    
-    return [NSURL URLWithString:urlStr];
+    return [NSURL URLWithString:[urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
 }
 
 -(void)getAtSize:(CGSize)size;
 {
     // Retrieves thumbnail of the picture of the day. Caches it and use the cached file next time.
-    CommonsApp *app = CommonsApp.singleton;
-    
-    NSError *err = nil;
-    NSData *urlData = [NSData dataWithContentsOfURL:[self getJsonUrl] options:nil error:&err];
-    if (err){
-        [self loadBundledDefaultPictureOfTheDay];
-        return;
-    }
-
-    err = nil;
-    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:urlData options:kNilOptions error:&err];
-    if (err){
-        [self loadBundledDefaultPictureOfTheDay];
-        return;
-    }
-
-    // Find the potdURL_ in the json data
-    NSString *urlStr = nil;
-    [self enumerateJSON:json currentKey:nil findKey:@"url" result:&urlStr];
-    if (!urlStr) {
-        NSLog(@"Could not locate Picture of the Day URL.");
-        [self loadBundledDefaultPictureOfTheDay];
-        return;
-    }
-
-    NSString *user = [self getUserFromJson:json];
-    NSLog(@"PotD from User = %@", user);
-    
-    NSDictionary *metadata = [self getMetadataFromJson:json];
-    NSLog(@"PotD Metadata = %@", metadata);
-    //NSLog(@"PotD Camera Model = %@", metadata[@"Model"]);
-    
-    // Determine the height and width of the picture of the day from the json data
-    CGSize originalSize = [self getSizeFromJson:json];
-    
-    // Uncomment to simulate extreme panorama
-    //originalSize.width = originalSize.width * 3;
-    
-    // Examine the reported width and height of the image so its aspect ratio can be
-    // calculated to see if it's a good fit. If it's an extremely wide panorama (vertical
-    // or horizontal) then it is not
-    if (![self isAspectRatioOkForSize:CGSizeMake(originalSize.width, originalSize.height)]) {
-        // If extreme panorama use bundled image
-        [self loadBundledDefaultPictureOfTheDay];
-        return;
-    }
-    
-    urlStr = [urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    potdURL_ = [NSURL URLWithString:urlStr];
-    //NSLog(@"potd json = %@", json);
-    
-    NSString *filename = [[potdURL_ path] lastPathComponent];
-    NSString *sizeKey = [NSString stringWithFormat:@"%dx%d", (int)size.width, (int)size.height];
-    
-    NSLog(@"FILE NAME = %@", filename);
-    NSString *key = [NSString stringWithFormat:@"%@-%@", sizeKey, filename];
-    UIImage *image = [thumbFetcher_ cachedThumbForKey:key];
-    if (image) {
-        // Cached image located. Use it.
-        self.done(image);
-    }else{
-        if (!filename) {
-            [self loadBundledDefaultPictureOfTheDay];
+    void (^retrievedJsonUrlData)(NSURLResponse*, NSData*, NSError*) = ^(NSURLResponse *response, NSData *urlData, NSError *err) {
+        if (err) return;
+        
+        err = nil;
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:urlData options:kNilOptions error:&err];
+        if (err) return;
+        
+        // Find the potdURL in the json data
+        NSString *urlStr = [self getValueForKey:@"url" fromJson:json];
+        
+        if (!urlStr) {
+            NSLog(@"Could not locate Picture of the Day URL.");
             return;
         }
+        
+        NSString *user = [self getValueForKey:@"user" fromJson:json];
+        NSLog(@"PotD from User = %@", user);
+        
+        NSDictionary *metadata = [self getMetadataFromJson:json];
+        NSLog(@"PotD Metadata = %@", metadata);
+        //NSLog(@"PotD Camera Model = %@", metadata[@"Model"]);
+        
+        // Determine the height and width of the picture of the day from the json data
+        // (The size needs to be know before the thumb is requested because the thumb needs
+        // to be sized to best fit the present device screen dimensions)
+        CGSize originalSize = [self getSizeFromJson:json];
+        
+        // Uncomment to simulate extreme panorama
+        //originalSize.width = originalSize.width * 3;
+        
+        // Examine the reported width and height of the image so its aspect ratio can be
+        // calculated to see if it's a good fit. If it's an extremely wide panorama (vertical
+        // or horizontal) then it is not
+        if (![self isAspectRatioOkForSize:CGSizeMake(originalSize.width, originalSize.height)]) return;
+        
+        urlStr = [urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        NSURL *potdURL = [NSURL URLWithString:urlStr];
+        //NSLog(@"potd json = %@", json);
+        
+        NSString *filename = [[potdURL path] lastPathComponent];
+        NSString *key = [self potdCacheFileName];
+        
+        if (!filename) return;
+        
         // Request the thumbnail be generated
-        MWApi *api = [app startApi];
+        MWApi *api = [CommonsApp.singleton startApi];
         
         // So it doesn't get double-encoded
         filename = [filename stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
@@ -135,37 +117,44 @@
         MWPromise *fetch = [api getRequest:params];
         [fetch done:^(NSDictionary *result) {
             //NSLog(@"potd fetch = %@", result);
+            
+            //NSString *thumbWidth = [self getValueForKey:@"thumbwidth" fromJson:result];
+            //NSString *thumbHeight = [self getValueForKey:@"thumbheight" fromJson:result];
 
-            /*
-            NSString *thumbWidth = nil;
-            NSString *thumbHeight = nil;
-            [self enumerateJSON:result currentKey:nil findKey:@"thumbwidth" result:&thumbWidth];
-            [self enumerateJSON:result currentKey:nil findKey:@"thumbheight" result:&thumbHeight];
-            */
-
-            NSDictionary *pages = result[@"query"][@"pages"];
-            for (NSString *pageId in pages) {
-                NSDictionary *page = pages[pageId];
-                NSDictionary *imageinfo = page[@"imageinfo"][0];
-                NSURL *potdThumbnailURL = [NSURL URLWithString:imageinfo[@"thumburl"]];
-                if (potdThumbnailURL) {
-                    // Now request the generated thumbnail
-                    MWPromise *fetchImage = [app fetchDataURL:potdThumbnailURL];
-                    [fetchImage done:^(NSData *data) {
-                        [thumbFetcher_ cacheImageData:data forKey:key];
-                        UIImage *image = [UIImage imageWithData:data scale:1.0];
-                        // Thumbnail retrieved. Use it.
-                        self.done(image);
-                    }];
-                    [fetchImage fail:^(NSError *error) {
-                        [self loadBundledDefaultPictureOfTheDay];
-                    }];
-                    [fetchImage progress:^(id arg) {
-                        
-                    }];
-                }
+            NSURL *potdThumbnailURL = [NSURL URLWithString:[self getValueForKey:@"thumburl" fromJson:result]];
+            if (potdThumbnailURL) {
+                // Now request the generated thumbnail
+                MWPromise *fetchImage = [CommonsApp.singleton fetchDataURL:potdThumbnailURL];
+                [fetchImage done:^(NSData *data) {
+                    [thumbFetcher_ cacheImageData:data forKey:key];
+                    UIImage *image = [UIImage imageWithData:data scale:1.0];
+                    // Thumbnail retrieved. Yay! Use it.
+                    self.done(image);
+                }];
+                [fetchImage fail:^(NSError *error) {
+                }];
+                [fetchImage progress:^(id arg) {
+                }];
             }
         }];
+    };
+    
+    UIImage *image = [thumbFetcher_ cachedThumbForKey:[self potdCacheFileName]];
+    if (image) {
+        // Cached image located. Use it.
+        self.done(image);
+    }else{
+        // Get json data for today's picture asynchronously
+        NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[self getJsonUrl]];
+        [NSURLConnection sendAsynchronousRequest:request
+                                           queue:[NSOperationQueue mainQueue]
+                               completionHandler:retrievedJsonUrlData];
+        /*
+        // Get json data for today's picture synchronously
+        NSError *err = nil;
+        NSData *urlData = [NSData dataWithContentsOfURL:[self getJsonUrl] options:nil error:&err];
+        retrievedJsonUrlData(nil, urlData, err);
+        */
     }
 }
 
@@ -190,67 +179,31 @@
     return result;
 }
 
--(void)loadBundledDefaultPictureOfTheDay
-{
-    UIImage *bundledDefaultPicOfDay = [UIImage imageNamed:@"Default-Pic-Of-Day.jpg"];
-    self.done(bundledDefaultPicOfDay);
-}
-
-// Based on: http://stackoverflow.com/a/15811405/135557
-// Could use some refactoring
-- (void)enumerateJSON:(id)object currentKey:(NSString *)currentKey findKey:(NSString *)soughtKey result:(NSString **)result
-{
-    if ([object isKindOfClass:[NSDictionary class]]){
-        // If it's a dictionary, enumerate it and pass in each key value to check
-        [object enumerateKeysAndObjectsUsingBlock:^(id key, id value, BOOL *stop) {
-            [self enumerateJSON:value currentKey:key findKey:soughtKey result:result];
-        }];
-    }else if ([object isKindOfClass:[NSArray class]]){
-        // If it's an array, pass in the objects of the array to check
-        [object enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-            [self enumerateJSON:obj currentKey:nil findKey:soughtKey result:result];
-        }];
-    }else{
-        if ([currentKey isEqualToString:soughtKey]) {
-            // If we got here (i.e. it's not a dictionary or array) so its a key/value that we needed
-            //urlStr = @"http://upload.wikimedia.org/wikipedia/foundation/9/9a/Wikimediafoundation-logo.png";
-            *result = (NSString *)object;
-        }
-    }
-}
-
 -(CGSize)getSizeFromJson:(NSDictionary *)json
 {
     // Determine the height and width of the picture of the day from the json data
-    NSString *origHeight = nil;
-    NSString *origWidth = nil;
-    [self enumerateJSON:json currentKey:nil findKey:@"height" result:&origHeight];
-    [self enumerateJSON:json currentKey:nil findKey:@"width" result:&origWidth];
-    CGSize originalSize = CGSizeZero;
-    if (origHeight && origWidth) {
-        originalSize = CGSizeMake(origWidth.floatValue, origHeight.floatValue);
-    }
-    return originalSize;
+    NSString *origHeight = [self getValueForKey:@"height" fromJson:json];
+    NSString *origWidth = [self getValueForKey:@"width" fromJson:json];
+    return (origHeight && origWidth) ? CGSizeMake(origWidth.floatValue, origHeight.floatValue) : CGSizeZero;
 }
 
--(NSString *)getUserFromJson:(NSDictionary *)json
+-(id)getValueForKey:(NSString *)key fromJson:(NSDictionary *)json
 {
-    NSString *user = nil;
-    [self enumerateJSON:json currentKey:nil findKey:@"user" result:&user];
-    return user;
+    id result = nil;
+    @try{
+        result = [[[json[@"query"][@"pages"] allValues] objectAtIndex:0][@"imageinfo"] objectAtIndex:0][key];
+    }@catch(id anException){
+        NSLog(@"Unexpected JSON structure!");
+    }
+    return result;
 }
 
 -(NSDictionary *)getMetadataFromJson:(NSDictionary *)json
 {
-    // As long as the structure is constant, could use this approach rather than "enumerateJSON:currentKey:findKey:result:" elsewhere
     NSMutableDictionary *result = [[NSMutableDictionary alloc] init];
-    @try{
-        NSArray *a = [[[json[@"query"][@"pages"] allValues] objectAtIndex:0][@"imageinfo"] objectAtIndex:0][@"metadata"];
-        for (NSDictionary* d in a) {
-            result[d[@"name"]] = d[@"value"];
-        }
-    }@catch(id anException){
-        NSLog(@"Unexpected JSON structure!");
+    NSArray *metadata = [self getValueForKey:@"metadata" fromJson:json];
+    for (NSDictionary* pair in metadata) {
+        result[pair[@"name"]] = pair[@"value"];
     }
     return result;
 }
